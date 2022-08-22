76d19397c92f6cfe275557ea52eaa55ea88dfd4d
Philip Dakowitz <dakowitz@subshell.com>
2022-08-22 11:59:06 +0200

    deps: update all dependencies to their latest version and increment version number to minor

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  rootDir: 'test'
+	preset: 'ts-jest',
+	testEnvironment: 'jsdom',
+	rootDir: 'test',
+	transformIgnorePatterns: ["/node_modules/(?!(dexie)/)"],
+	moduleNameMapper: {
+		"^dexie$": require.resolve("dexie"),
+	}
 };
